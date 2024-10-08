768d4cfa9a34552ec88911a61bd2093529dcc643
Jay Ahn <jay@xerapix.studio>
2024-10-08 10:21:58 +0900

    Week 03 Initialize

---
+++ week03_concert/src/test/java/com/example/concert/ConcertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
