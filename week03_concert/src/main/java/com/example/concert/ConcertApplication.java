9b7702cabc22ab5eb65025e4341d4529b920361f
Jay Ahn <jay@xerapix.studio>
2024-11-14 15:04:19 +0900

    docs: added STEP16_REPORT.MD

--- week03_concert/src/main/java/com/example/concert/ConcertApplication.java
+++ week03_concert/src/main/java/com/example/concert/ConcertApplication.java
@@ -3,7 +3,9 @@ package com.example.concert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @EnableCaching
 @SpringBootApplication
 public class ConcertApplication {
